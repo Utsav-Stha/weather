beb183f643a9d028d2b76f8db50210fd00f204b0
Utsav Shrestha <shrestha8502@gmail.com>
2024-01-25 18:21:09 +0545

    Create basic layout of UI

--- lib/features/home_page/home_page.dart
+++ lib/features/home_page/home_page.dart
@@ -5,6 +5,82 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'WEATHER APP',
+          style: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 1.6,
+            wordSpacing: 3.0,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    flex: 9,
+                    child: TextField(),
+                  ),
+                  Expanded(
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.search),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.location_on_sharp),
+                  Text('Kathmandu'),
+                ],
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              Text('24 C'),
+              SizedBox(
+                height: 20.0,
+              ),
+              Icon(
+                Icons.cloudy_snowing,
+                size: 140.0,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.60,
+                child: GridView.builder(
+                  // physics: NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                  ),
+                  itemCount: 4,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.all(10.0),
+                      color: Colors.red,
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
